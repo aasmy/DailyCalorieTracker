3a5b1d2fc0ceb96aaf1ece7a5912057aab468284
Abdullah Asmy <abdallaasmi14@gmail.com>
2025-11-02 15:12:51 -0800

    Add Main class as the entry point for the Calorie Tracker application

--- src/code/com/asmy/calorietracker/Main.java
+++ src/code/com/asmy/calorietracker/Main.java
@@ -1,6 +1,24 @@
 package com.asmy.calorietracker;
 
+/**
+ * Entry point of the Daily Calorie Tracker application.
+ * Initializes the CalorieTracker and starts the program.
+ *
+ * @author Abdullah Asmy
+ * @version 1.0
+ */
 public class Main
 {
+    /**
+     * Main method - entry point for the application.
+     *
+     * @param args command-line arguments
+     */
+    public static void main(final String[] args)
+    {
+        CalorieTracker tracker;
+        tracker = new CalorieTracker();
 
+        tracker.start();
+    }
 }
